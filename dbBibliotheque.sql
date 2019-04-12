f152ab97e664a0e6e8634ea6cf7bee8022363f3e
Isaac David Zolana <isaacworld2@hotmail.com>
2019-04-12 17:21:22 -0400

    Modification du fichier dbBibliotheque.sql : Ajout de certaines informations manquantes

--- dbBibliotheque.sql
+++ dbBibliotheque.sql
@@ -1,17 +1,17 @@
 create table Artistes(
-
 	id int PRIMARY KEY AUTO_INCREMENT,
-	nomArtiste varchar(255),
-	isMembre boolean
-
+	nom varchar(255),
+	isMembre boolean,
+	photo varchar(255)
 );
 
 create table Albums(
-
-	id int,
+	id int PRIMARY KEY AUTO_INCREMENT,
+	id_Artiste int,
 	titre varchar(255),
 	genre varchar(255),
+	anneeSortie date,
+	imageCouverture varchar(255),
 	CONSTRAINT check_genre
   		CHECK (genre IN ('Classique', 'Hip-Hop', 'Rock', 'Folk', 'Jazz'))
-
 );
